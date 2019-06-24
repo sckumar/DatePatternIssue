3eb547199cb0796ffbc01ff610f0f5964b9b9e8d
senthil_sethuram <senthil.sethuram@intellectdesign.com>
2019-06-24 23:06:25 +0530

    issue reproduced

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-globalization.GlobalizationError",
+    "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+    "pluginId": "cordova-plugin-globalization",
+    "clobbers": [
+      "window.GlobalizationError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-globalization.globalization",
+    "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+    "pluginId": "cordova-plugin-globalization",
+    "clobbers": [
+      "navigator.globalization"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-globalization": "1.11.0"
+};
+// BOTTOM OF METADATA
+});
